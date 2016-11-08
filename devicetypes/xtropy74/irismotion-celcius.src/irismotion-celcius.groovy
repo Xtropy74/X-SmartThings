20a34bc806b2f6de12b9cd74da4e44e807358960
Xtropy74 <xtropy@gmail.com>
2016-11-08 12:34:48 -0500

    Update irismotion-celcius.groovy

--- devicetypes/xtropy74/irismotion-celcius.src/irismotion-celcius.groovy
+++ devicetypes/xtropy74/irismotion-celcius.src/irismotion-celcius.groovy
@@ -287,7 +287,7 @@ private Map getTemperatureResult(value) {
 		name: 'temperature',
 		value: value,
 		descriptionText: descriptionText,
-        	translatable: true,
+		translatable: true,
 		unit: temperatureScale
 	]
 }
